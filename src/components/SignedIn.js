73949196c7e48b49bf1b4c2896471d81a8f704af
Matz-dev1 <mzapolski0@gmail.com>
2025-03-29 11:53:14 +0100

    feat: signedIn page for smaller devices

--- src/components/SignedIn.js
+++ src/components/SignedIn.js
@@ -1,3 +1,4 @@
+import "./SignedIn.css";
 import { useState } from "react";
 import iconList from "../images/icon-list.svg";
 import ButtonSubmit from "./ButtonSubmit";
@@ -9,51 +10,21 @@ function SignedIn({ buttonText }) {
     setIsVisible(false);
   };
 
-  if (!isVisible) {
-    return null;
-  } else {
-    return (
-      <div
-        style={{
-          width: "28%",
-          margin: "0 auto",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          className="background"
-          style={{
-            backgroundColor: "white",
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            minWidth: "350px",
-            boxSizing: "border-box",
-            marginTop: "30%",
-            borderRadius: "5%",
-            paddingTop: "6vh",
-            paddingLeft: "8vh",
-            paddingRight: "8vh",
-            paddingBottom: "6vh",
-          }}
-        >
-          <img
-            src={iconList}
-            alt="icon check"
-            style={{ width: "7vh", height: "7vh" }}
-          />
-          <h2 style={{ fontSize: "2.8em", margin: "1vh" }}>
-            Thanks for subscribing!
-          </h2>
-          <p>
-            A confirmation email has been sent to <b>{email}</b>. Please open it
-            and click the button inside to confirm your subscribtion.
-          </p>
-          <ButtonSubmit buttonText={buttonText} onClick={toggleDisplay} />
-        </div>
+  if (!isVisible) return null;
+
+  return (
+    <div className="signedInContainer">
+      <div className="background">
+        <img src={iconList} alt="icon check" className="icon" />
+        <h2 className="title">Thanks for subscribing!</h2>
+        <p className="message">
+          A confirmation email has been sent to <b>{email}</b>. Please open it
+          and click the button inside to confirm your subscription.
+        </p>
+        <ButtonSubmit buttonText={buttonText} onClick={toggleDisplay} />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 export default SignedIn;
